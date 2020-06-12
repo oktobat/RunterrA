3293fa6704fbfb7eb40f203be1920586f170794d
oktobat <tsalt@hanmail.net>
2020-06-12 10:31:25 +0900

    6월 12일 수정

--- js/myscript.js
+++ js/myscript.js
@@ -8,8 +8,8 @@ $(".heros").slick({
     autoplaySpeed: 2500,          // 간격시간
     dots: true,                               // 동그라미 버튼, 기본값이 false로 설정
     speed: 600,                            // 슬라이드 동작하는데 걸리는 시간(생략가능)
-    centerMode:true,                  // 좌우측에 다다음슬라이드가 살짝비치는것
-    centerPadding:"90px",     
+    centerMode: true,                  // 좌우측에 다다음슬라이드가 살짝비치는것
+    centerPadding: "90px",
     slidesToShow: 1,                   // 보여지는 슬라이드 수(생략가능)
     slidesToScroll: 1,                   // 넘어가는 슬라이드 수(생략가능)
     pauseOnHover: true,          //  슬라이드 위에 마우스 over을할시 슬라이드 멈춤여부  false는 안멈춤(생략가능)
@@ -21,61 +21,70 @@ $(".heros").slick({
     arrows: true,                         //  기본값은 true이며  슬라이드 좌우측 방향 버튼(생략가능)
     prevArrow: '<button class="prevArrow marrow"><i class="fas fa-angle-left"></i></button>',
     nextArrow: '<button class="nextArrow marrow"><i class="fas fa-angle-right"></i></button>',
-})         
+})
 
 
 $(".heros .plpa").toggle(
-    function(){
-            $(".heros").slick("slickPause")
-            $(this).find("i").removeClass("fa-pause").addClass("fa-play")
+    function () {
+        $(".heros").slick("slickPause")
+        $(this).find("i").removeClass("fa-pause").addClass("fa-play")
     },
-    function(){
-            $(".heros").slick("slickPlay")
-            $(this).find("i").removeClass("fa-play").addClass("fa-pause")
+    function () {
+        $(".heros").slick("slickPlay")
+        $(this).find("i").removeClass("fa-play").addClass("fa-pause")
     }
 )
 
 
-$(window).scroll(function(){
+$(window).scroll(function () {
 
     var sct = $(this).scrollTop()
-   if(sct > 1500) {
-       $(".cards1").css({
-           left:"0%"
-       })
-   } else {
-    $(".cards1").css({
-        left:"-100%"
-    })
-   }
-   if(sct > 2000) {
-    $(".cards2").css({
-        right:"0%"
-    })
-} else {
- $(".cards2").css({
-     right:"-100%"
- })
-}
+    if (sct > 1500) {
+        $(".cards1").css({
+            left: "0%"
+        })
+    } else {
+        $(".cards1").css({
+            left: "-100%"
+        })
+    }
+    if (sct > 2000) {
+        $(".cards2").css({
+            right: "0%"
+        })
+    } else {
+        $(".cards2").css({
+            right: "-100%"
+        })
+    }
 })
 
-$(".openMOgnb").on("click", function(){
+$(".openMOgnb").on("click", function () {
     $("#top").addClass("on")
     $(this).hide()
     $(".closeMOgnb").show()
 })
 
-$(".closeMOgnb").on("click", function(){
+$(".closeMOgnb").on("click", function () {
     $("#top").removeClass("on")
     $(this).hide()
     $(".openMOgnb").show()
 })
 
 
-$(".play2 a").on("click", function(e){
+$(".play2 a").on("click", function (e) {
     e.preventDefault()
-   var src = $(this).find("img").attr("src")
-   $(".popupBox").addClass("on")
-   $(".popupBox .inner  img").attr("src", src)
+    var mobileTop = $(".mobiles").offset().top
+    var src = $(this).find("img").attr("src")
+    $(".popupBox").addClass("on")
+    $(".popupBox .inner").css({
+        top: mobileTop + 200,
+        transform: "translate(-50%, 0%)"
+    })
+    $(".popupBox .inner  img").attr("src", src)
 
+})
+
+$(".popupBox .inner button").on("click", function () {
+    $(".popupBox").removeClass("on")
 })
